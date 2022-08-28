9f1f90f3b89783756dda41da63fa4c3716587542
FredonRa <fede_99_leiva@hotmail.com>
2022-08-28 11:13:16 -0300

    add weather screen

---
+++ src/icons.ts
@@ -0,0 +1,20 @@
+import gps from "./img/gps.png"
+import marker from "./img/marker.png"
+import thermometer from "./img/thermometer.png"
+import italy from "./img/italy.png"
+import france from "./img/france.png"
+import unitedKingdom from "./img/united_kingdom.png"
+import ireland from "./img/ireland.png"
+import netherlands from "./img/netherlands.png"
+
+
+export const Icons = {
+    gps: gps,
+    marker: marker,
+    thermometer: thermometer,
+    italy: italy,
+    france: france,
+    unitedKingdom: unitedKingdom,
+    ireland: ireland,
+    netherlands: netherlands
+}
